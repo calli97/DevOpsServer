1bf284034884e44d2dea41943b43f9899aac7f69
calli97 <nahuelcallalli@gmail.com>
2025-09-21 20:05:33 -0300

    add db options to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,4 +5,12 @@ dotenv.config();
 export default {
   port: process.env.PORT,
   githubWebhookSecret: process.env.GITHUB_WEBHOOK_SECRET,
+  database: {
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
 };
