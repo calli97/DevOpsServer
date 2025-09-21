d66466916164a0c165ad4ace8885913084f3b74f
calli97 <nahuelcallalli@gmail.com>
2025-09-21 15:09:02 -0300

    log request

--- src/controller/GitHubWebhookController.ts
+++ src/controller/GitHubWebhookController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 class GitHubWebhookController {
   static async receiveWebhook(req: Request, res: Response) {
+    console.log(req);
     res.status(200).json({ ok: true });
   }
 }
