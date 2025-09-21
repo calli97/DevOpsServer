5f058a9dca73fc300918bba9033cacba27c51ec4
calli97 <nahuelcallalli@gmail.com>
2025-09-21 13:16:28 -0300

    add basic response

--- src/controller/GitHubWebhookController.ts
+++ src/controller/GitHubWebhookController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 
 class GitHubWebhookController {
-  static async receiveWebhook(req: Request, res: Response) {}
+  static async receiveWebhook(req: Request, res: Response) {
+    res.status(200).json({ ok: true });
+  }
 }
 
 export default GitHubWebhookController;
