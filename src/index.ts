cf0835d0ced477f8267090f6c21e6d7dfe572013
Nahuel Callalli <nahuel@venturing.com.ar>
2025-09-21 13:06:06 -0300

    add helmet

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./routes";
 import config from "./config";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
