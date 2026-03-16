156491f0372348ae7bc6ab13d24d8db3fc54b861
calli97 <nahuelcallalli@gmail.com>
2026-03-16 17:36:30 -0300

    fix get controller

--- MasterServer/src/routes/github.ts
+++ MasterServer/src/routes/github.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { GitHubWebhookController } from "../controller/GitHubWebhookController";
-import { ProjectService } from "../service/ProjectService";
+import { ProjectInstanceService } from "../service/ProjectInstanceService";
 import { DeployService } from "../service/DeployService";
 import { ConfigFileService } from "../service/ConfigFileService";
 import { GitHubService } from "../service/GitHubService";
 import PM2Service from "../service/PM2Service";
 import { getRepository } from "../dbConnection";
-import Project from "../entity/Project";
 import Deploy from "../entity/Deploy";
 
 const router = Router();
@@ -15,14 +14,13 @@ let controller: GitHubWebhookController;
 
 async function getController() {
   if (!controller) {
-    const projectRepository = await getRepository(Project);
     const deployRepository = await getRepository(Deploy);
     const pm2Service = new PM2Service();
     const deployService = new DeployService(deployRepository, pm2Service);
     const configFileService = new ConfigFileService();
     const githubService = new GitHubService();
-    const projectService = new ProjectService(projectRepository, deployService, configFileService, githubService);
-    controller = new GitHubWebhookController(projectService);
+    const projectInstanceService = new ProjectInstanceService(deployService, configFileService, githubService);
+    controller = new GitHubWebhookController(projectInstanceService);
   }
   return controller;
 }
