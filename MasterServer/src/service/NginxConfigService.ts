588755f5616194a87576ee869fd1045fe7216796
calli97 <nahuelcallalli@gmail.com>
2026-03-23 15:50:55 -0300

    add write nginx config function

--- MasterServer/src/service/NginxConfigService.ts
+++ MasterServer/src/service/NginxConfigService.ts
@@ -1,6 +1,9 @@
+import * as fs from "fs/promises";
+import * as path from "path";
 import NginxConfig from "../entity/NginxConfig";
 import { getRepository } from "../dbConnection";
 import { NotFoundError } from "../errors/AppError";
+import { logger } from "./LogService";
 
 export class NginxConfigService {
   async findByProjectInstance(instanceId: number): Promise<NginxConfig[]> {
@@ -33,5 +36,11 @@ export class NginxConfigService {
     const repository = await getRepository(NginxConfig);
     const result = await repository.delete(id);
     return (result.affected ?? 0) > 0;
+  }
+
+  async writeFile(nginxConfig: NginxConfig): Promise<void> {
+    const filePath = path.join(nginxConfig.path, nginxConfig.name);
+    await fs.writeFile(filePath, nginxConfig.content, "utf-8");
+    logger.info(`[NginxConfigService] Written: ${filePath}`);
   }
 }
